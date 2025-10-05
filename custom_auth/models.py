0ab4b9027e0baec2f106de388ca9eedc25b075d9
Your Name <you@example.com>
2025-10-05 06:19:26 +0600

    fix Bucket oper

--- custom_auth/models.py
+++ custom_auth/models.py
@@ -63,7 +63,7 @@ class Order(models.Model):
     created_date = models.DateTimeField()
     products = models.JSONField(default=list)
     total_price = models.PositiveIntegerField(default=0)
-    user = models.ForeignKey(Profile,on_delete=models.CASCADE,related_name='orders')
+    user = models.ForeignKey(Profile,on_delete=models.CASCADE)
     client_number = models.CharField(max_length=30)
 
 
